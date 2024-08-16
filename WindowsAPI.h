a5d3a06bafaf0c7778dde7db8c77d95c067dd9b6
takuma <k023g0015@g.neec.ac.jp>
2024-08-16 16:45:42 +0900

    03_02 完了

--- WindowsAPI.h
+++ WindowsAPI.h
@@ -1,7 +1,11 @@
 #pragma once
+#include <windows.h>
 
 class WindowsAPI
 {
+public://静的メンバ関数
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
 public://メンバ関数
 	//初期化
 	void Initialize();
