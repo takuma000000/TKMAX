d692df5a82109a93e015474e2aadf8a2f2c75154
takuma <k023g0015@g.neec.ac.jp>
2024-09-30 17:27:15 +0900

    完了

--- Input.cpp
+++ Input.cpp
@@ -8,7 +8,6 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 	HRESULT result;
 
 	//DirectInputのインスタンス作成
-	ComPtr<IDirectInput8> directInput = nullptr;
 	result = DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
 	assert(SUCCEEDED(result));
 	//キーボードデバイス生成
@@ -24,9 +23,31 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 
 void Input::Update()
 {
+	//前回のキー入力を保存
+	memcpy(keyPre, key, sizeof(key));
+
 	//キーボード情報の取得開始
 	keyboard->Acquire();
 	//全キーの入力情報を取得する
-	BYTE key[256] = {};
 	keyboard->GetDeviceState(sizeof(key), key);
+}
+
+bool Input::PushKey(BYTE keyNumber)
+{
+	//指定キーを押していればtrueを返す
+	if (key[keyNumber]) {
+		return true;
+	}
+	//そうでなければfalseで返す
+	return false;
+}
+
+bool Input::TriggerKey(BYTE keyNumber)
+{
+	//指定キーを押していればtrueを返す
+	if (keyPre[keyNumber]) {
+		return true;
+	}
+	//そうでなければfalseで返す
+	return false;
 }
--- Input.h
+++ Input.h
@@ -15,10 +15,20 @@ public://メンバ関数
 	void Initialize(HINSTANCE hInstance, HWND hwnd);
 	//更新
 	void Update();
+	//キーの押下をチェック( 押されているか )
+	bool PushKey(BYTE keyNumber);
+	//キーのトリガーをチェック
+	bool TriggerKey(BYTE keyNumber);
 
 private://メンバ変数
 	//キーボードのデバイス
 	ComPtr<IDirectInputDevice8> keyboard;
+	//全キーの状態
+	BYTE key[256] = {};
+	//前回の全キーの状態
+	BYTE keyPre[256] = {};
+	//DirectInputのインスタンス
+	ComPtr<IDirectInput8> directInput;
 
 };
 
