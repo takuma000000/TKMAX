e02b48a1d09b57bc6d4f792cc0a0ac38097fc39b
takuma <k023g0015@g.neec.ac.jp>
2024-10-01 17:18:14 +0900

    途中

--- DirectXCommon.cpp
+++ DirectXCommon.cpp
@@ -1,5 +1,42 @@
 #include "DirectXCommon.h"
+#include <cassert>
+#include <format>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+using namespace Microsoft::WRL;
+
 
 void DirectXCommon::Initialize()
 {
+}
+
+void DirectXCommon::InitializeDevice()
+{
+	HRESULT hr;
+
+	Microsoft::WRL::ComPtr<ID3D12Device> device = nullptr;
+	//機能レベルとログ出力用の文字列
+	D3D_FEATURE_LEVEL featureLevels[] = {
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+	};
+
+	const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
+	//高い順に生成できるか試していく
+	for (size_t i = 0; _countof(featureLevels); ++i) {
+		//採用したアダプターでデバイスを生成
+		hr = D3D12CreateDevice(useAdapter.Get(), featureLevels[i], IID_PPV_ARGS(&device));
+		//指定した機能レベルでデバイスが生成できたかを確認
+		if (SUCCEEDED(hr)) {
+			//生成できたのでログ出力を行って
+			Log(std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+	//デバイスの生成がうまくいかなかったので起動できない
+	assert(device != nullptr);
+	//初期化完了のログを出す
+	Log("Complet create D3D12Device!!!\n");
+
 }
