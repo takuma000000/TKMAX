492b79d4b4cb3c6a6f381b676d0912b84b0fc0c3
takuma <k023g0015@g.neec.ac.jp>
2024-10-23 14:08:27 +0900

    完了

--- DirectXCommon.cpp
+++ DirectXCommon.cpp
@@ -74,6 +74,9 @@ Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> CreateDescriptorHeap(
 
 void DirectXCommon::Initialize(WindowsAPI* windowsAPI)
 {
+	//FPS初期化固定
+	InitializeFixFPS();
+
 	//NULL検出
 	assert(windowsAPI);
 
@@ -552,6 +555,39 @@ void DirectXCommon::InitializeImGui()
 	);
 }
 
+void DirectXCommon::InitializeFixFPS()
+{
+
+	//現在時間を記録する
+	reference_ = std::chrono::steady_clock::now();
+
+}
+
+void DirectXCommon::UpdateFixFPS()
+{
+
+	// 1/60秒ぴったりの時間
+	const std::chrono::microseconds kMinTime(uint64_t(1000000.0f / 60.0f));
+	// 1/60秒よりわずかに短い時間
+	const std::chrono::microseconds kMinCheckTime(uint64_t(1000000.0f / 65.0f));
+
+	//現在時間を取得する
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+	//前回記録からの経過時間を取得
+	std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - reference_);
+	// 1/60秒( よりわずかに短い時間 )経っていない場合
+	if (elapsed < kMinCheckTime) {
+		// 1/60秒経過するまで微小なスリープを繰り返す
+		while (std::chrono::steady_clock::now() - reference_ < kMinTime) {
+			//1マイクロ秒スリープ
+			std::this_thread::sleep_for(std::chrono::microseconds(1));
+		}
+	}
+	//現在の時間を記録する
+	reference_ = std::chrono::steady_clock::now();
+
+}
+
 D3D12_CPU_DESCRIPTOR_HANDLE DirectXCommon::GetSRVCPUDescriptorHandle(uint32_t index)
 {
 	return GetCPUDescriptorHandle(srvDescriptorHeap.Get(), descriptorSizeSRV, index);
@@ -643,6 +679,9 @@ void DirectXCommon::PostDraw()
 	fenceValue++;
 	hr = commandQueue->Signal(fence.Get(), fenceValue);
 	assert(SUCCEEDED(hr));
+
+	//FPS固定
+	UpdateFixFPS();
 
 	// GPUがコマンドリストの実行を終了するまで待つ
 	if (fence->GetCompletedValue() < fenceValue) {
