15240b816140aae3f5e8507fa1b4abb5192a6c30
takuma <k023g0015@g.neec.ac.jp>
2024-10-28 17:10:50 +0900

    10ページの途中まで

--- Sprite.cpp
+++ Sprite.cpp
@@ -1,5 +1,27 @@
 #include "Sprite.h"
+#include "SpriteCommon.h"
+#include "DirectXCommon.h"
 
-void Sprite::Initialize()
-{
+void Sprite::Initialize(SpriteCommon* spriteCommon,DirectXCommon* dxCommon){
+	//引数で受け取ったメンバ変数に記録する
+	this->spriteCommon = spriteCommon;
+	dxCommon_ = dxCommon;
+
+	//VertexResourceを作る
+	vertexResource = dxCommon_->CreateBufferResource(sizeof(VertexData) * 6);
+	//IndexResourceを作る
+	indexResource = dxCommon_->CreateBufferResource(sizeof(uint32_t) * 6);
+	//VertexBufferViewを作成する
+	//Resourceの先頭のアドレスから使う
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	//使用するResourceのサイズは頂点6つ分のサイズ
+	vertexBufferView.SizeInBytes = sizeof(VertexData) * 6;
+	//1頂点あたりのサイズ
+	vertexBufferView.StrideInBytes = sizeof(VertexData);
+	//リソースの先頭のアドレスから使う
+	indexBufferView.BufferLocation = indexResource->GetGPUVirtualAddress();
+	//使用するリソースのサイズはインデックス6つ分のサイズ
+	indexBufferView.SizeInBytes = sizeof(uint32_t) * 6;
+	//インデックスはuint32_tとする
+	indexBufferView.Format = DXGI_FORMAT_R32_UINT;
 }
--- Sprite.h
+++ Sprite.h
@@ -1,8 +1,60 @@
 #pragma once
+#include "DirectXCommon.h"
+
+class SpriteCommon;
+
 class Sprite
 {
 
-public://メンバ関数...初期化
-	void Initialize();
+	struct Vector2 {
+		float x;
+		float y;
+	};
+
+	struct Vector3 {
+		float x;
+		float y;
+		float z;
+	};
+
+	struct Vector4 {
+		float x;
+		float y;
+		float z;
+		float w;
+	};
+
+	//頂点データ
+	struct VertexData {
+		Vector4 position;
+		Vector2 texcoord;
+		Vector3 normal;
+	};
+
+	struct MaterialData {
+		std::string textureFilePath;
+	};
+
+	struct ModelData {
+		std::vector<VertexData> vertices;
+		MaterialData material;
+	};
+
+public://メンバ関数
+	void Initialize(SpriteCommon* spriteCommon, DirectXCommon* dxCommon);
+
+private:
+	SpriteCommon* spriteCommon = nullptr;
+	DirectXCommon* dxCommon_;
+
+	//バッファリソース
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource = nullptr;;
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexResource = nullptr;
+	//バッファリソース内のデータを指すポインタ
+	VertexData* vertexData = nullptr;
+	uint32_t* indexData = nullptr;
+	//バッファリソースの使い道を補足するバッファビュー
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+	D3D12_INDEX_BUFFER_VIEW indexBufferView{};
 
 };
