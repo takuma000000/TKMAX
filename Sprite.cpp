1909a8bd27cf816f17a37eca5efd8bc362c52bde
takuma <k023g0015@g.neec.ac.jp>
2024-10-28 17:30:30 +0900

    14ページまで

--- Sprite.cpp
+++ Sprite.cpp
@@ -2,6 +2,25 @@
 #include "SpriteCommon.h"
 #include "DirectXCommon.h"
 
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+Matrix4x4 MakeIdentity4x4() {
+
+	Matrix4x4 identity;
+
+	// 単位行列を生成する
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			identity.m[i][j] = (i == j) ? 1.0f : 0.0f;
+		}
+	}
+
+	return identity;
+
+}
+
 void Sprite::Initialize(SpriteCommon* spriteCommon,DirectXCommon* dxCommon){
 	//引数で受け取ったメンバ変数に記録する
 	this->spriteCommon = spriteCommon;
@@ -26,6 +45,31 @@ void Sprite::Initialize(SpriteCommon* spriteCommon,DirectXCommon* dxCommon){
 	indexBufferView.Format = DXGI_FORMAT_R32_UINT;
 	//
 	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	//左下
+	vertexData[0].position = { 0.0f,360.0f,0.0f,1.0f };
+	vertexData[0].texcoord = { 0.0f,1.0f };
+	vertexData[0].normal = { 0.0f,0.0f,-1.0f };
+	//左上
+	vertexData[1].position = { 0.0f,0.0f,0.0f,1.0f };
+	vertexData[1].texcoord = { 0.0f,0.0f };
+	vertexData[1].normal = { 0.0f,0.0f,-1.0f };
+	//右上
+	vertexData[2].position = { 640.0f,360.0f,0.0f,1.0f };
+	vertexData[2].texcoord = { 1.0f,1.0f };
+	vertexData[2].normal = { 0.0f,0.0f,-1.0f };
+	//2枚目の三角形
+	//左上
+	vertexData[3].position = { 0.0f,0.0f,0.0f,1.0f };
+	vertexData[3].texcoord = { 0.0f,0.0f };
+	vertexData[3].normal = { 0.0f,0.0f,-1.0f };
+	//右上
+	vertexData[4].position = { 640.0f,0.0f,0.0f,1.0f };
+	vertexData[4].texcoord = { 1.0f,0.0f };
+	vertexData[4].normal = { 0.0f,0.0f,-1.0f };
+	//右下
+	vertexData[5].position = { 640.0f,360.0f,0.0f,1.0f };
+	vertexData[5].texcoord = { 1.0f,1.0f };
+	vertexData[5].normal = { 0.0f,0.0f,-1.0f };
 	//
 	indexResource->Map(0, nullptr, reinterpret_cast<void**>(&indexData));
 	indexData[0] = 0;
@@ -34,4 +78,12 @@ void Sprite::Initialize(SpriteCommon* spriteCommon,DirectXCommon* dxCommon){
 	indexData[3] = 1;
 	indexData[4] = 3;
 	indexData[5] = 2;
+	//マテリアうリソースを作る
+	materialResource = dxCommon_->CreateBufferResource(sizeof(Material));
+	//書き込むためのアドレスを取得
+	materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
+	//今回は白を書き込んでみる
+	materialData->color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+	materialData->enableLighting = true;
+	materialData->uvTransform = MakeIdentity4x4();
 }
