7f4dc175f520d88cddf71ab7615cf9b64af18a9e
takuma <k023g0015@g.neec.ac.jp>
2024-11-11 14:59:52 +0900

    12ページから

--- Sprite.cpp
+++ Sprite.cpp
@@ -56,20 +56,26 @@ void Sprite::Initialize(SpriteCommon* spriteCommon, DirectXCommon* dxCommon, std
 }
 
 void Sprite::Update() {
+
+	float left = 0.0f - anchorPoint.x;
+	float right = 1.0f - anchorPoint.x;
+	float top = 0.0f - anchorPoint.y;
+	float bottom = 1.0f - anchorPoint.y;
+
 	//左下
-	vertexData[0].position = { 0.0f,1.0f,0.0f,1.0f };
+	vertexData[0].position = { left,bottom,0.0f,1.0f };
 	vertexData[0].texcoord = { 0.0f,1.0f };
 	vertexData[0].normal = { 0.0f,0.0f,-1.0f };
 	//左上
-	vertexData[1].position = { 0.0f,0.0f,0.0f,1.0f };
+	vertexData[1].position = { left,top,0.0f,1.0f };
 	vertexData[1].texcoord = { 0.0f,0.0f };
 	vertexData[1].normal = { 0.0f,0.0f,-1.0f };
 	//右下
-	vertexData[2].position = { 1.0f,1.0f,0.0f,1.0f };
+	vertexData[2].position = { right,bottom,0.0f,1.0f };
 	vertexData[2].texcoord = { 1.0f,1.0f };
 	vertexData[2].normal = { 0.0f,0.0f,-1.0f };
 	//右上
-	vertexData[3].position = { 1.0f,0.0f,0.0f,1.0f };
+	vertexData[3].position = { right,top,0.0f,1.0f };
 	vertexData[3].texcoord = { 1.0f,0.0f };
 	vertexData[3].normal = { 0.0f,0.0f,-1.0f };
 
