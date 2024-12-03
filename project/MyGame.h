fab856d2b278166f17913814d2029b49a44c1762
takuma <k023g0015@g.neec.ac.jp>
2024-12-03 15:25:20 +0900

    完了

--- project/MyGame.h
+++ project/MyGame.h
@@ -30,5 +30,38 @@ public://メンバ関数
 	//描画
 	void Draw();
 
+	bool IsEndRequest() const { return endRequest_; } // 終了リクエスト確認
+
+private://メンバ変数
+	//ポインタ...WindowsAPI
+	std::unique_ptr<WindowsAPI> windowsAPI = nullptr;
+	//Audio
+	std::unique_ptr<AudioManager> audio = nullptr;
+	//ポインタ...Input
+	std::unique_ptr<Input> input = nullptr;
+	//ポインタ...DirectXCommon
+	std::unique_ptr<DirectXCommon> dxCommon = nullptr;
+	//ポインタ...srvManager
+	std::unique_ptr<SrvManager> srvManager = nullptr;
+	//ポインタ...SpriteCommon
+	std::unique_ptr<SpriteCommon> spriteCommon = nullptr;
+	//ポインタ...Sprite
+	std::unique_ptr<Sprite> sprite = nullptr;
+	//ポインタ...Object3dCommon
+	std::unique_ptr<Object3dCommon> object3dCommon = nullptr;
+	//ポインタ...Camera
+	std::unique_ptr<Camera> camera = nullptr;
+	//ポインタ...ImGuiManager
+	std::unique_ptr<ImGuiManager>  imguiManager = nullptr;
+
+	//Object3dの初期化
+	Object3d* object3d = new Object3d();
+	//AnotherObject3d ( もう一つのObject3d )
+	Object3d* anotherObject3d = new Object3d();
+
+private:
+	//フラグ
+	bool endRequest_ = false; // 終了フラグ
+
 };
 
