c73ad1c476f5e9ad41abda2406568597b38ec5c8
takuma <k023g0015@g.neec.ac.jp>
2024-12-03 14:55:14 +0900

    完了

--- project/MyGame.cpp
+++ project/MyGame.cpp
@@ -1,7 +1,105 @@
 #include "MyGame.h"
 
+#include <wrl.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
 void MyGame::Initialize()
 {
+	//基盤システムの初期化*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+	//ポインタ...WindowsAPI
+	std::unique_ptr<WindowsAPI> windowsAPI = nullptr;
+	//WindowsAPIの初期化
+	windowsAPI = std::make_unique<WindowsAPI>();
+	windowsAPI->Initialize();
+
+	//Audio
+	std::unique_ptr<AudioManager> audio = nullptr;
+	audio = std::make_unique<AudioManager>();
+	audio->Initialize();
+
+	//ポインタ...Input
+	std::unique_ptr<Input> input = nullptr;
+	//入力の初期化
+	input = std::make_unique<Input>();
+	input->Initialize(windowsAPI.get());
+
+	//ポインタ...DirectXCommon
+	std::unique_ptr<DirectXCommon> dxCommon = nullptr;
+	//DirectXの初期化
+	dxCommon = std::make_unique<DirectXCommon>();
+	dxCommon->Initialize(windowsAPI.get());
+	//XAudioのエンジンのインスタンスを生成
+	audio->LoadSound("fanfare", "fanfare.wav");
+	// 音声の再生
+	audio->PlaySound("fanfare");
+
+	//ポインタ...srvManager
+	std::unique_ptr<SrvManager> srvManager = nullptr;
+	srvManager = std::make_unique<SrvManager>();
+	srvManager->Initialize(dxCommon.get());
+
+	//テクスチャマネージャの初期化
+	TextureManager::GetInstance()->Initialize(dxCommon.get(), srvManager.get());
+	//ファイルパス
+	TextureManager::GetInstance()->LoadTexture("./resources/uvChecker.png");
+	TextureManager::GetInstance()->LoadTexture("./resources/pokemon.png");
+
+	//ポインタ...SpriteCommon
+	std::unique_ptr<SpriteCommon> spriteCommon = nullptr;
+	//スプライト共通部の初期化
+	spriteCommon = std::make_unique<SpriteCommon>();
+	spriteCommon->Initialize(dxCommon.get());
+
+	//ポインタ...Sprite
+	std::unique_ptr<Sprite> sprite = nullptr;
+	sprite = std::make_unique<Sprite>();
+	sprite->Initialize(spriteCommon.get(), dxCommon.get(), "./resources/uvChecker.png");
+
+	//ポインタ...Object3dCommon
+	std::unique_ptr<Object3dCommon> object3dCommon = nullptr;
+	//Object3d共通部の初期化
+	object3dCommon = std::make_unique<Object3dCommon>();
+	object3dCommon->Initialize(dxCommon.get());
+
+
+	ModelManager::GetInstance()->Initialize(dxCommon.get());
+	ModelManager::GetInstance()->LoadModel("axis.obj", dxCommon.get());
+
+	///--------------------------------------------
+
+	//Object3dの初期化
+	Object3d* object3d = new Object3d();
+	object3d->Initialize(object3dCommon.get(), dxCommon.get());
+	object3d->SetModel("axis.obj");
+
+	//AnotherObject3d ( もう一つのObject3d )
+	Object3d* anotherObject3d = new Object3d();
+	anotherObject3d->Initialize(object3dCommon.get(), dxCommon.get());
+	anotherObject3d->SetModel("plane.obj");
+
+	//---------------------------------------------
+
+	//ポインタ...Camera
+	std::unique_ptr<Camera> camera = nullptr;
+	//Object3d共通部の初期化
+	camera = std::make_unique<Camera>();
+	camera->SetRotate({ 0.0f,0.0f,0.0f });
+	camera->SetTranslate({ 0.0f,0.0f,-30.0f });
+	//object3dCommon->SetDefaultCamera(camera.get());
+	object3d->SetCamera(camera.get());
+	anotherObject3d->SetCamera(camera.get());
+	//ImGui用のcamera設定
+	Vector3 cameraPosition = camera->GetTranslate();
+	Vector3 cameraRotation = camera->GetRotate();
+
+	//ポインタ...ImGuiManager
+	std::unique_ptr<ImGuiManager>  imguiManager = nullptr;
+	imguiManager = std::make_unique<ImGuiManager>();
+	imguiManager->Initialize(windowsAPI.get(), dxCommon.get());
+
+	//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 }
 
 void MyGame::Finalize()
--- project/MyGame.h
+++ project/MyGame.h
@@ -1,4 +1,23 @@
 #pragma once
+
+// GE3クラス化(MyClass)
+#include "WindowsAPI.h"
+#include "Input.h"
+#include "DirectXCommon.h"
+#include "SpriteCommon.h"
+#include "engine/2d/Sprite.h"
+#include "TextureManager.h"
+#include "Object3dCommon.h"
+#include "Object3d.h"
+#include "Model.h"
+#include "ModelCommon.h"
+#include "ModelManager.h"
+#include "Camera.h"
+#include "SrvManager.h"
+#include "ImGuiManager.h"
+#include "AudioManager.h"
+
+
 class MyGame
 {
 public://メンバ関数
