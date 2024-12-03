aabd7ec88b851e007deb609d5275ad908cb6f523
takuma <k023g0015@g.neec.ac.jp>
2024-12-03 14:40:27 +0900

    完了

--- project/AudioManager.cpp
+++ project/AudioManager.cpp
@@ -30,14 +30,18 @@ void AudioManager::Finalize() {
 }
 
 bool AudioManager::LoadSound(const std::string& key, const std::string& filename) {
-	if (soundMap.find(key) != soundMap.end()) {
-		return false; // 既にロード済み
-	}
+    if (soundMap.find(key) != soundMap.end()) {
+        return false; // 既にロード済み
+    }
 
-	SoundData soundData = LoadWaveFile(filename);
-	soundMap[key] = soundData;
-	return true;
+    // ファイル名に "resources/" を追加
+    std::string fullPath = "resources/" + filename;
+
+    SoundData soundData = LoadWaveFile(fullPath);
+    soundMap[key] = soundData;
+    return true;
 }
+
 
 void AudioManager::PlaySound(const std::string& key) {
 	auto it = soundMap.find(key);
@@ -61,7 +65,16 @@ void AudioManager::PlaySound(const std::string& key) {
 
 	hr = sourceVoice->Start();
 	assert(SUCCEEDED(hr));
+
+	// 音声の終了を待つ
+	XAUDIO2_VOICE_STATE state;
+	do {
+		sourceVoice->GetState(&state);
+	} while (state.BuffersQueued > 0);
+
+	sourceVoice->DestroyVoice();
 }
+
 
 void AudioManager::UnloadSound(const std::string& key) {
 	auto it = soundMap.find(key);
@@ -72,49 +85,44 @@ void AudioManager::UnloadSound(const std::string& key) {
 }
 
 SoundData AudioManager::LoadWaveFile(const std::string& filename) {
-	std::ifstream file(filename, std::ios::binary);
+	std::ifstream file(filename, std::ios_base::binary);
 	assert(file.is_open());
 
-	// RIFFヘッダー
-	char riffHeader[12];
-	file.read(riffHeader, 12);
-	assert(std::memcmp(riffHeader, "RIFF", 4) == 0);
-	assert(std::memcmp(riffHeader + 8, "WAVE", 4) == 0);
-
-	// FMTチャンク
-	char fmtHeader[8];
-	file.read(fmtHeader, 8);
-	assert(std::memcmp(fmtHeader, "fmt ", 4) == 0);
+	RiffHeader riff;
+	file.read(reinterpret_cast<char*>(&riff), sizeof(riff));
+	assert(strncmp(riff.chunk.id, "RIFF", 4) == 0);
+	assert(strncmp(riff.type, "WAVE", 4) == 0);
 
 	FormatChunk format = {};
-	file.read(reinterpret_cast<char*>(&format.fmt), sizeof(WAVEFORMATEX));
+	file.read(reinterpret_cast<char*>(&format.chunk), sizeof(ChunkHeader));
+	assert(strncmp(format.chunk.id, "fmt ", 4) == 0);
+	file.read(reinterpret_cast<char*>(&format.fmt), format.chunk.size);
 
-	// Dataチャンク
-	ChunkHeader dataChunk;
-	file.read(reinterpret_cast<char*>(&dataChunk), sizeof(dataChunk));
-	while (std::memcmp(dataChunk.id, "data", 4) != 0) {
+	// PCM形式か確認
+	assert(format.fmt.wFormatTag == WAVE_FORMAT_PCM);
 
-		file.read((char*)&dataChunk, sizeof(dataChunk));
-
-		// Dataチャンクを検出した場合
-		if (strncmp(dataChunk.id, "data", 4) == 0) {
-			break;  // Dataチャンクが見つかったのでループを抜ける
+	ChunkHeader data;
+	while (true) {
+		file.read(reinterpret_cast<char*>(&data), sizeof(data));
+		if (file.eof() || !file) {
+			assert(false); // Dataチャンクが見つからなかった
 		}
-
-		file.seekg(dataChunk.size, std::ios::cur);
-		file.read(reinterpret_cast<char*>(&dataChunk), sizeof(dataChunk));
+		if (strncmp(data.id, "data", 4) == 0) {
+			break;
+		}
+		file.seekg(data.size, std::ios_base::cur);
 	}
 
-	char* pBuffer = new char[dataChunk.size];
-	file.read(pBuffer, dataChunk.size);
-
+	char* pBuffer = new char[data.size];
+	file.read(pBuffer, data.size);
 	file.close();
 
-	SoundData soundData = {};
+	SoundData soundData;
 	soundData.wfex = format.fmt;
 	soundData.pBuffer = reinterpret_cast<BYTE*>(pBuffer);
-	soundData.bufferSize = dataChunk.size;
-
+	soundData.bufferSize = data.size;
 
 	return soundData;
 }
+
+
