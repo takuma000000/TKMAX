20f4011c1d9d2268b494323d7945178ca5da25f5
takuma <k023g0015@g.neec.ac.jp>
2024-12-09 14:12:17 +0900

    実行通るようにした

--- project/engine/base/DirectXCommon.cpp
+++ project/engine/base/DirectXCommon.cpp
@@ -4,6 +4,7 @@
 #include <format>
 #include "Logger.h"
 #include "StringUtility.h"
+#include "thread"
 //#include "externals/imgui/imgui.h"
 //#include "externals/imgui/imgui_impl_win32.h"
 //#include "externals/imgui/imgui_impl_dx12.h"
