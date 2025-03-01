0cd2e57ef3e1fb232bd9da5c276b9ed9ce923812
takuma <k023g0015@g.neec.ac.jp>
2025-03-01 12:02:40 +0900

    コメント修正

--- project/engine/base/WindowsAPI.cpp
+++ project/engine/base/WindowsAPI.cpp
@@ -69,7 +69,7 @@ void WindowsAPI::Initialize()
 	//ウィンドウの生成
 	hwnd = CreateWindow(
 		wc.lpszClassName,		//利用するクラス名
-		L"TKMAX",					//タイトルバーの文字( なんでも良い )
+		L"TKMAX",				//タイトルバーの文字( なんでも良い )
 		WS_OVERLAPPEDWINDOW,	//ウィンドウスタイル
 		CW_USEDEFAULT,			//表示X座標(Windowsに任せる)
 		CW_USEDEFAULT,			//表示Y座標(WindowsOSに任せる)
