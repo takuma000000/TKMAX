5a857ebb59d975e6993ebd06519fa9285ef2d26c
takuma <k023g0015@g.neec.ac.jp>
2025-05-23 13:05:38 +0900

    imguiファイルの追加と色リストの更新
    imgui関連の新しいソースファイル（imgui.cpp、imgui_demo.cpp、imgui_draw.cpp、imgui_tables.cpp、imgui_widgets.cpp）を追加し、imgui.vcxprojのパスを更新しました。バイナリファイルも変更されています。
    
    また、ParticleManager.cppのMakeNewParticle関数において、色のリストに新しい色を追加し、既存の色を保持しました。

--- project/engine/effect/ParticleManager.cpp
+++ project/engine/effect/ParticleManager.cpp
@@ -436,7 +436,10 @@ ParticleManager::Particle ParticleManager::MakeNewParticle(std::mt19937& randomE
 		{0.2f, 0.8f, 1.0f, 1.0f},
 		{1.0f, 1.0f, 0.5f, 1.0f},
 		{1.0f, 0.7f, 0.2f, 1.0f},
-		{0.6f, 1.0f, 0.6f, 1.0f}
+		{0.6f, 1.0f, 0.6f, 1.0f},
+		{0.5f, 0.5f, 1.0f, 1.0f},
+		{1.0f, 0.5f, 1.0f, 1.0f},
+		{1.0f, 0.84f, 0.0f, 1.0f}
 	};
 
 	Particle particle;
